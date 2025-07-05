b1475a5dffea9fdd3c1aa34f18da8772b7d8ad78
LaminThiri <154306004+LaThiri17@users.noreply.github.com>
2025-07-06 02:19:24 +0700

    Update vite.config.js

--- vat-calculator/vite.config.js
+++ vat-calculator/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-vat-calculator/',
-  plugins: [react()],
+  base: '/vat-calculator/',   // Important for GitHub Pages subfolder
+  plugins: [react()]
 })
